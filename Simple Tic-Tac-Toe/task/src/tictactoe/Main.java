dad6982e1d494d4fae4a1b148e33435a9297806a
affzkwn <afifzakwanzaili@gmail.com>
2021-10-09 02:05:56 +0800

    restructure logic. The logic kind of mess so need to redo the codes. Things need to be done is make a logic that check every single grid and make do while loop function properly

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -21,8 +21,9 @@ public class Main {
             System.out.println("| " + gridMatrix[i][0] + " " + gridMatrix[i][1] + " " + gridMatrix[i][2] + " |");
         }
         System.out.println("---------");
-        LoopGridAvail(gridMatrix);
-
+        updateGridPlayer1(gridMatrix);
+        updateGridPlayer2(gridMatrix);
+        return;
     }
 
     static void updateGridPlayer1(char[][] gridMatrix){
@@ -93,137 +94,7 @@ public class Main {
         }
         System.out.println("---------");
     }
-    static void LoopGridAvail(char[][] gridMatrix){
-        boolean winner = false;
-        do {
-            updateGridPlayer1(gridMatrix);
-            checkResult(gridMatrix);
-            updateGridPlayer2(gridMatrix);
-            int X = 0;
-            int O = 1;
-            int floor = 0;                                           //to ensure that the grid is 3x3.
-            for (int i = 0; i < 3; i++) {
-
-                if (gridMatrix[0][0] == gridMatrix[0][1] && gridMatrix[0][1] == gridMatrix[0][2]) {
-                    System.out.println(gridMatrix[0][0]);
-                    if(gridMatrix[0][0]!= "_")
-                    winner = true;
-                    return;
-                }
-                else if (gridMatrix[1][0] == gridMatrix[1][1] && gridMatrix[1][1] == gridMatrix[1][2]) {
-                    System.out.println(gridMatrix[1][0]);
-                    winner = true;
-                    return;
-                }
-                else if (gridMatrix[2][0] == gridMatrix[2][1] && gridMatrix[2][1] == gridMatrix[2][2]) {
-                    System.out.println(gridMatrix[2][0]);
-                    winner = true;
-                    return;
-                }
-                //Check Vertical Result
-                else if (gridMatrix[0][0] == gridMatrix[1][0] && gridMatrix[1][0] == gridMatrix[2][0]) {
-                    System.out.println(gridMatrix[0][0]);
-                    winner = true;
-                    return;
-                }
-                else if (gridMatrix[0][1] == gridMatrix[1][1] && gridMatrix[1][1] == gridMatrix[2][1]) {
-                    System.out.println(gridMatrix[0][1]);
-                    winner = true;
-                    return;
-                }
-                else if (gridMatrix[0][2] == gridMatrix[1][2] && gridMatrix[1][2] == gridMatrix[2][2]) {
-                    System.out.println(gridMatrix[0][2]);
-                    winner = true;
-                    return;
-                }                                                       //Check Diagonal Result
-                else if (gridMatrix[0][0] == gridMatrix[1][1] && gridMatrix[1][1] == gridMatrix[2][2]) {
-                    System.out.println(gridMatrix[0][0]);
-                    winner = true;
-                    return;
-                }
-                else if (gridMatrix[0][2] == gridMatrix[1][1] && gridMatrix[1][1] == gridMatrix[2][0]) {
-                    System.out.println(gridMatrix[0][2]);
-                    winner = true;
-                    return;
-                }else
-                floor++;
-            }
-
-        }while(!winner);
-        return;
-
+    static void logicCheck(char[][] gridMatrix){
 
     }
-    static void checkResult(char[][] gridMatrix) {
-        boolean winner = false;
-        int X = 0;
-        int O = 1;
-        int floor = 0;                                           //to ensure that the grid is 3x3.
-        for (int i = 0; i < 3; i++) {
-
-            if (gridMatrix[0][0] == gridMatrix[0][1] && gridMatrix[0][1] == gridMatrix[0][2]) {
-                System.out.println(gridMatrix[0][0]);
-                winner = true;
-                return;
-            }
-            if (gridMatrix[1][0] == gridMatrix[1][1] && gridMatrix[1][1] == gridMatrix[1][2]) {
-                System.out.println(gridMatrix[1][0]);
-                winner = true;
-                return;
-            }
-            if (gridMatrix[2][0] == gridMatrix[2][1] && gridMatrix[2][1] == gridMatrix[2][2]) {
-                System.out.println(gridMatrix[2][0]);
-                winner = true;
-                return;
-            }
-            //Check Vertical Result
-            if (gridMatrix[0][0] == gridMatrix[1][0] && gridMatrix[1][0] == gridMatrix[2][0]) {
-                System.out.println(gridMatrix[0][0]);
-                winner = true;
-                return;
-            }
-            if (gridMatrix[0][1] == gridMatrix[1][1] && gridMatrix[1][1] == gridMatrix[2][1]) {
-                System.out.println(gridMatrix[0][1]);
-                winner = true;
-                return;
-            }
-            if (gridMatrix[0][2] == gridMatrix[1][2] && gridMatrix[1][2] == gridMatrix[2][2]) {
-                System.out.println(gridMatrix[0][2]);
-                winner = true;
-                return;
-            }                                                       //Check Diagonal Result
-            if (gridMatrix[0][0] == gridMatrix[1][1] && gridMatrix[1][1] == gridMatrix[2][2]) {
-                System.out.println(gridMatrix[0][0]);
-                winner = true;
-                return;
-            }
-            if (gridMatrix[0][2] == gridMatrix[1][1] && gridMatrix[1][1] == gridMatrix[2][0]) {
-                System.out.println(gridMatrix[0][2]);
-                winner = true;
-                return;
-            }
-            floor++;
-        }
-
-                                                                //Check Row Result
-
-        //Calculate either there is more O than X or more X than O
-        //int result = X > O ? X - O : O - X;
-        //if(result > 1 ) {
-        //    System.out.println("Impossible");
-        //    return;                                             //restart the process without go through other condition.
-        //}
-        //if(winner.length() == 1 || winner != "_"  ){                         //win can happen before the game finished.
-        //    System.out.println(winner + " wins");//Check logic condition to meet the requirement.
-        //}//else if (winner.length() > 1) {
-          //  System.out.println("Impossible");
-        //}
-        //else if (floor > 2) {                                  //floor should be max 3 to make the grid become 3x3
-        //    System.out.println("Game not finished");
-        //}else
-        //    System.out.println("Draw");
-        //    return;
-    }
-
 }
-
